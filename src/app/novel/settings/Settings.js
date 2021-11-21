afe3eeabcabd1bf82afdb768916ac789fe7f2e86
unknown <uzairaslam490@gmail.com>
2021-11-21 20:28:22 +0500

    Afsanay

--- src/app/novel/settings/Settings.js
+++ src/app/novel/settings/Settings.js
@@ -1,29 +1,90 @@
 /* eslint-disable prettier/prettier */
-import * as React from 'react';
-import {View, Text} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+/* eslint-disable react-native/no-inline-styles */
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import Slider from '@react-native-community/slider';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { COLOR_PRIMARY } from '../../colors';
 
-export default class Settings extends React.Component {
-  constructor(props) {
+export default class Page extends Component {
+  constructor(props){
     super(props);
-    this.props.navigation;
+    this.fontChanged = this.fontChanged.bind(this);
+    this.fontFamilyChanged = this.fontFamilyChanged.bind(this);
+  }
+  componentDidMount(){
+
+  }
+  fontChanged(val){
+    let { setNovel } = this.props;
+    setNovel({
+      fontSize: val,
+    });
+  }
+  fontFamilyChanged(itemValue, itemIndex){
+    let { setNovel } = this.props;
+    setNovel({
+      fontFamily: itemValue,
+    });
   }
   render() {
-    // const {navigation} = this.props;
-    //   if (navigation.isFocused()){
-    //   navigation.setOptions({tabBarIcon:() => {
-    //     return <Icon name="settings-outline" size={25}/>;
-    //   }});}
-    //   else {
-    //     navigation.setOptions({tabBarIcon:() => {
-    //       return <Icon name="settings" size={25}/>;
-    //     }});
-    //   }
+    let { isFetching, error, no, novel, fontSize, fontFamily } = this.props;
     return (
-      // eslint-disable-next-line react-native/no-inline-styles
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Let's Get Started...</Text>
+      <View style={styles.mainContainer}>
+        <ScrollView>
+          <View style={styles.oneSetting}>
+            <View style={{ flex: 1, flexDirection: 'row', paddingHorizontal: 16, paddingVertical: 4, marginBottom: 10 }}>
+              <Text style={{ flex: 1, fontSize: 18 }}>Size</Text>
+              <Text style={{ flex: 1, textAlign: 'right', fontSize: 18 }}>{fontSize}</Text>
+            </View>
+            <Slider
+              thumbTintColor={COLOR_PRIMARY}
+              minimumTrackTintColor={COLOR_PRIMARY}
+              maximumValue={40}
+              minimumValue={10}
+              step={1}
+              value={fontSize}
+              onValueChange={this.fontChanged}
+            />
+          </View>
+          <View style={styles.oneSetting}>
+            <View style={{ flex: 1, flexDirection: 'row', paddingHorizontal: 16, paddingVertical: 4 }}>
+              <Text style={{ flex: 1, fontSize: 18 }}>Font</Text>
+            </View>
+            <View style={{ paddingHorizontal: 8 }}>
+              <Picker
+                mode={'dropdown'}
+                selectedValue={fontFamily}
+                onValueChange={this.fontFamilyChanged}>
+                <Picker.Item label="Regular" value="NotoNaskhArabic-Regular" />
+                <Picker.Item label="Bold" value="NotoNaskhArabic-Bold" />
+              </Picker>
+            </View>
+          </View>
+        </ScrollView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  oneSetting: {
+    flexDirection: 'column',
+    margin: 10,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    elevation: 5,
+  },
+});
--- src/app/novel/settings/index.js
+++ src/app/novel/settings/index.js
@@ -1,5 +1,26 @@
 /* eslint-disable prettier/prettier */
+import { connect } from 'react-redux';
 import Settings from './Settings';
+import {
+  setNovel,
+} from '../actions';
 
+const mapStateToProps = (store) => {
+  return {
+    no: store.page.no,
+    text: store.page.text,
+    isFetching: store.page.isFetching,
+    error: store.page.error,
+    novel: store.novel.novel,
+    fontSize: store.novel.fontSize,
+    fontFamily: store.novel.fontFamily,
+  };
+};
 
-export default Settings;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setNovel: (params) => dispatch(setNovel(params)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Settings);
